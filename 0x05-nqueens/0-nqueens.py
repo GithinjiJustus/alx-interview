eba01e63479b806399af9af42386a49b384aed29
GithinjiJustus <justusgithinji257@gmail.com>
2024-08-28 23:58:03 -0700

    chess

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,71 +1,91 @@
 #!/usr/bin/python3
+'''N Queens Challenge'''
+
 import sys
 
-def print_solution(board):
-    """Print the solution in the required format"""
-    solution = []
-    for row in range(len(board)):
-        for col in range(len(board)):
-            if board[row][col] == 1:
-                solution.append([row, col])
-    print(solution)
 
-def is_safe(board, row, col):
-    """Check if a queen can be placed on board[row][col]"""
-    # Check the current column
-    for i in range(row):
-        if board[i][col] == 1:
-            return False
-
-    # Check the upper-left diagonal
-    i, j = row, col
-    while i >= 0 and j >= 0:
-        if board[i][j] == 1:
-            return False
-        i -= 1
-        j -= 1
-
-    # Check the upper-right diagonal
-    i, j = row, col
-    while i >= 0 and j < len(board):
-        if board[i][j] == 1:
-            return False
-        i -= 1
-        j += 1
-
-    return True
-
-def solve_nqueens(board, row):
-    """Solve the N Queens problem using backtracking"""
-    if row == len(board):
-        print_solution(board)
-        return
-
-    for col in range(len(board)):
-        if is_safe(board, row, col):
-            board[row][col] = 1
-            solve_nqueens(board, row + 1)
-            board[row][col] = 0  # Backtrack
-
-def main():
-    """Main function to handle input and start solving the N Queens problem"""
+if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
 
     try:
-        N = int(sys.argv[1])
+        n = int(sys.argv[1])
     except ValueError:
-        print("N must be a number")
-        sys.exit(1)
+        print('N must be a number')
+        exit(1)
 
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
+    if n < 4:
+        print('N must be at least 4')
+        exit(1)
 
-    # Initialize the chessboard
-    board = [[0 for _ in range(N)] for _ in range(N)]
-    solve_nqueens(board, 0)
+    solutions = []
+    placed_queens = []  # coordinates format [row, column]
+    stop = False
+    r = 0
+    c = 0
 
-if __name__ == "__main__":
-    main()
+    # iterate thru rows
+    while r < n:
+        goback = False
+        # iterate thru columns
+        while c < n:
+            # check is current column is safe
+            safe = True
+            for cord in placed_queens:
+                col = cord[1]
+                if(col == c or col + (r-cord[0]) == c or
+                        col - (r-cord[0]) == c):
+                    safe = False
+                    break
+
+            if not safe:
+                if c == n - 1:
+                    goback = True
+                    break
+                c += 1
+                continue
+
+            # place queen
+            cords = [r, c]
+            placed_queens.append(cords)
+            # if last row, append solution and reset all to last unfinished row
+            # and last safe column in that row
+            if r == n - 1:
+                solutions.append(placed_queens[:])
+                for cord in placed_queens:
+                    if cord[1] < n - 1:
+                        r = cord[0]
+                        c = cord[1]
+                for i in range(n - r):
+                    placed_queens.pop()
+                if r == n - 1 and c == n - 1:
+                    placed_queens = []
+                    stop = True
+                r -= 1
+                c += 1
+            else:
+                c = 0
+            break
+        if stop:
+            break
+        # on fail: go back to previous row
+        # and continue from last safe column + 1
+        if goback:
+            r -= 1
+            while r >= 0:
+                c = placed_queens[r][1] + 1
+                del placed_queens[r]  # delete previous queen coordinates
+                if c < n:
+                    break
+                r -= 1
+            if r < 0:
+                break
+            continue
+        r += 1
+
+    for idx, val in enumerate(solutions):
+        if idx == len(solutions) - 1:
+            print(val, end='')
+        else:
+            print(val)
